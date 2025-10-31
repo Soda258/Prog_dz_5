36dca6768171c3c7d3273aa5e83b10ebcc1b265b
Семёнов Дмитрий Юрьевич <semenovdmitry28@gmail.com>
2025-10-31 22:26:21 +0700

    d5

--- src/array_function.c
+++ src/array_function.c
@@ -27,8 +27,8 @@ void join_and_sort_int_arrays(int* src1, size_t size_src1, int* src2, size_t siz
     }
 }
 void get_min_and_max_from_int_array(int* src, size_t size, int* min, int* max) {
-    int* min = src[0];
-    int* max = src[0];
+    *min = src[0];
+    *max = src[0];
 
     for (int i = 1; i < size; i++) {
         if (src[i] < *min) {*min = src[i];}
