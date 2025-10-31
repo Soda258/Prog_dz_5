0f13c2146ca010311fa8686490df7445d7f90c53
Семёнов Дмитрий Юрьевич <semenovdmitry28@gmail.com>
2025-10-31 22:21:17 +0700

    d3

--- src/array_function.c
+++ src/array_function.c
@@ -8,8 +8,22 @@ void join_int_arrays(int* src1, size_t size_src1, int* src2, size_t size_src2, i
         dest[size_src1 + i] = src2[i];
     }
 }
-
-void join_and_sort_int_arrays(int* src1, size_t size_src1, int* src2, size_t size_src2, int* dest) {}
-
+void join_and_sort_int_arrays(int* src1, size_t size_src1, int* src2, size_t size_src2, int* dest) {
+    for(int i = 0; i < size_src1; i++) {
+        dest[i] = src1[i];
+    }
+    for(int i = 0; i < size_src2; i++) {
+        dest[size_src1 + i] = src2[i];
+    }
+    int t = 0;
+    for(int i = 0; i < size_src1 + size_src2; i++){
+        for(int j = i + 1; j < size_src1 + size_src2; j++){
+            if(dest[i] > dest[j]){
+                t = dest[i];
+                dest[i] = dest[j];
+                dest[j] = t;
+            }
+        }
+    }
+}
 void get_min_and_max_from_int_array(int* src, size_t size, int* min, int* max) {}
-
